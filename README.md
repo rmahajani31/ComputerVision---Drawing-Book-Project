2934c04b5092f9012102247e49ac4f1d900b20e0
aignatius3 <aignatius3@gatech.edu>
2019-12-04 17:59:55 -0500

    running step experiments

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 This project looks at sketches and generates the best shapes and steps to create the sketch.
 
 # Run Instructions
-python getShapes.py inputImageName
+python drawImage.py inputImageName
 
-ex. python getShapes.py bicycle
+ex. python drawImage.py bicycle
--- drawImage.py
+++ drawImage.py
@@ -15,16 +15,17 @@ else:
 img = cv2.imread('input/' + imageName + '.png')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+step_num = 0
+
 print("Getting & Grouping Lines")
 drawn_img = np.ones(img.shape)
 a, b = lineOrder(img=img,n_steps=3)
 for i in range(len(a)):
     for j in range(len(a[i])):
+        step_num += 1
         drawn_img[a[i][j],b[i][j]] = 0
-        cv2.imshow('image', drawn_img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
         drawn_img = eraseStrict(img, drawn_img)
+        cv2.imwrite('steps/' + imageName + '_step'  + str(step_num) + '.png', drawn_img*255)
 
 print("Getting & Grouping Circles")
 top_circles, include_arr = getCircles(img, drawn_img)
@@ -32,15 +33,15 @@ circle_clusters = circleOrder(top_circles, include_arr)
 
 print("Drawing Contours")
 for cluster in circle_clusters:
+    print(len(cluster))
+    step_num += 1
     for circle in cluster:
         drawContour(img, drawn_img, (circle[0], circle[1]), circle[2], 5)
     drawn_img = eraseStrict(img, drawn_img)
-    cv2.imshow('image', drawn_img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    cv2.imwrite('steps/' + imageName + '_step'  + str(step_num) + '.png', drawn_img*255)
 
 print("Saving Image")
-cv2.imwrite('output/' + imageName + '_output.png', drawn_img)
+cv2.imwrite('output/' + imageName + '_output.png', drawn_img*255)
 cv2.imshow('image', drawn_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
